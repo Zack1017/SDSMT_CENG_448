c70625e602ae6ffd503157e48319a45ea3c6d4e5
Zackery Holloway <Zackery Holloway@mines.sdsmt.edu>
2026-02-02 15:30:38 -0700

    Clean up .gitignore and remove build artifacts from tracking

--- Lab_2/CMakeFiles/extra-clean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/extra-clean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/extra-clean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
